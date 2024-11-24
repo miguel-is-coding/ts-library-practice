b5b8472f480ccc74853cd8ffa61a7a1b8f048d95
Miguel Ángel Pérez García <miguel.perez@leanmind.es>
2024-11-24 15:19:17 +0000

    feat: split tests into two suites, add coverage collector and threshold.

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,15 @@ module.exports = {
   transform: {
     "^.+.tsx?$": ["ts-jest",{}],
   },
+  verbose: true,
+  collectCoverage: true,
+  coverageDirectory: "./coverage",
+  coverageThreshold: {
+    global: {
+      statements: 75,
+      branches: 75,
+      functions: 75,
+      lines: 75
+    }
+  }
 };
