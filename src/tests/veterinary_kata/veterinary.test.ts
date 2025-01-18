b053bc421e187b6675ecd44ac1bb123f9b02fd50
Miguel Ángel Pérez García <miguel.perez@leanmind.es>
2025-01-18 16:24:54 +0000

    refactor: apply builder pattern in veterinary.test.

--- src/tests/veterinary_kata/veterinary.test.ts
+++ src/tests/veterinary_kata/veterinary.test.ts
@@ -1,4 +1,4 @@
-import {DiseaseFilter} from "../../veterinary_kata/veterinary";
+import {Case, Diagnosis, DiseaseFilter} from "../../veterinary_kata/veterinary";
 
 describe('Disease filter', () => {
     let diseaseFilter: DiseaseFilter;
@@ -6,19 +6,20 @@ describe('Disease filter', () => {
     it('filters cases when several diagnosis filters are applied together', () => {
         let expectedName1 = 'Chupito';
         let expectedName2 = 'Juliana';
-        let cases = [
-            createCase(1, expectedName1),
-            createCase(2, expectedName2),
-            createCase(3, 'irrelevantName'),
-        ];
+        createCase(1, expectedName1);
+        createCase(2, expectedName2);
+        createCase(3, 'irrelevantName');
         let searchCriterion1 = 'Vías respiratorias altas';
         let searchCriterion2 = 'Cerebro';
-        let diagnoses = [
-            createDiagnosis(1, searchCriterion1),
-            createDiagnosis(2, searchCriterion2),
-            createDiagnosis(3, 'Oídos'),
-        ];
-        diseaseFilter = DiseaseFilter.create(cases, diagnoses);
+        const fixtures = casesWithDiagnoses()
+            .havingDiagnosisWithLocationAndCaseWithName(searchCriterion1, expectedName1)
+            .havingDiagnosisWithLocationAndCaseWithName(searchCriterion2, expectedName2)
+            .havingDiagnosisWithLocationAndCaseWithName('irrelevantLocation', 'irrelevantName')
+            .build()
+        createDiagnosis(1, searchCriterion1);
+        createDiagnosis(2, searchCriterion2);
+        createDiagnosis(3, 'Oídos');
+        diseaseFilter = DiseaseFilter.create(fixtures.cases, fixtures.diagnoses);
         diseaseFilter.addFilter(searchCriterion2);
         diseaseFilter.addFilter(searchCriterion1);
 
@@ -29,6 +30,32 @@ describe('Disease filter', () => {
         expect(result[0].patientName).toBe(expectedName2);
     });
 });
+
+function casesWithDiagnoses() {
+    let diagnosesId = 0;
+    let diagnoses: Diagnosis[] = [];
+    let cases: Case[] = [];
+
+    const add = (location: string, patientName: string) => {
+        diagnosesId++;
+        diagnoses.push(createDiagnosis(diagnosesId, location));
+        cases.push(createCase(diagnosesId, patientName));
+    }
+
+    const builder = {
+        havingDiagnosisWithLocationAndCaseWithName: (location: string, patientName: string) => {
+            add(location, patientName);
+            return builder;
+        },
+
+        build: () => ({
+            cases: cases,
+            diagnoses: diagnoses,
+        })
+    }
+
+    return builder;
+}
 
 function createDiagnosis(id: number, location: string) {
     return {
