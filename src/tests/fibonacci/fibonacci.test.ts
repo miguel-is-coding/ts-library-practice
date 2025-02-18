55d5a2973c31a8dcdb54b14c7ac60ac51d29d49c
Miguel Angel Perez Garcia <miguelangel.perez.dev@gmail.com>
2025-02-18 06:54:00 +0000

    red: yields zero when the value is zero.

---
+++ src/tests/fibonacci/fibonacci.test.ts
@@ -0,0 +1,9 @@
+function fibonacci(number: number) {
+
+}
+
+describe('Fibonacci should', () => {
+    test('yields zero when the value is zero', () => {
+        expect(fibonacci(0)).toBe(0);
+    });
+});
