24647e686680a4b7bee567ddaca3f3c803f5da49
Miguel Angel Perez Garcia <miguelangel.perez.dev@gmail.com>
2025-02-18 06:57:10 +0000

    red: yields one to number one.

--- src/tests/fibonacci/fibonacci.test.ts
+++ src/tests/fibonacci/fibonacci.test.ts
@@ -6,4 +6,8 @@ describe('Fibonacci should', () => {
     test('yields zero when the value is zero', () => {
         expect(fibonacci(0)).toBe(0);
     });
+
+    test('yields one to number one', () => {
+        expect(fibonacci(1)).toBe(1);
+    });
 });
