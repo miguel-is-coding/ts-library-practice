a25f641602e326774b12c4793dece6d4119e744d
Miguel Angel Perez Garcia <miguelangel.perez.dev@gmail.com>
2025-02-20 08:18:41 +0000

    green: finds the prime composition of the given number.

--- src/tests/prime_factors/primeFactors.test.ts
+++ src/tests/prime_factors/primeFactors.test.ts
@@ -1,27 +1,16 @@
 function primeFactorsOf(number: number) {
     let factor = 2;
-    if (number % factor !== 0) {
-        factor = 3
+    while (number % factor !== 0) {
+        ++factor;
     }
     const factors = [factor];
     const remainder = number / factor;
     if (remainder > 1) {
-        return factors.concat(primeFactorsOf(remainder))
+        return factors.concat(primeFactorsOf(remainder));
     }
     return factors;
 }
 
-/**
- * Cases:
- * x 2 -> 2
- * x 4 -> [2,2]
- * x 8 -> [2,2,2]
- * x 3 -> [3]
- * 9 -> [3,3]
- * 6 -> [2,3]
- * 25 -> [5,5]
- * 1155 -> [3,5,7,11]
- */
 describe('Prime factors', () => {
     it('finds the prime composition of the given number', () => {
         expect(primeFactorsOf(2)).toEqual([2]);
@@ -31,5 +20,6 @@ describe('Prime factors', () => {
         expect(primeFactorsOf(9)).toEqual([3, 3]);
         expect(primeFactorsOf(6)).toEqual([2, 3]);
         expect(primeFactorsOf(25)).toEqual([5, 5]);
+        expect(primeFactorsOf(1155)).toEqual([3, 5, 7, 11]);
     });
 });
