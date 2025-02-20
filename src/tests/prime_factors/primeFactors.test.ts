7bb074479e55aa3695cba3df3b260f993e1c70e8
Miguel Angel Perez Garcia <miguelangel.perez.dev@gmail.com>
2025-02-20 08:25:58 +0000

    ref: separate test cases and remove redundant assertions.

--- src/tests/prime_factors/primeFactors.test.ts
+++ src/tests/prime_factors/primeFactors.test.ts
@@ -1,14 +1,16 @@
 import {primeFactorsOf} from "../../prime_factors/primeFactors";
 
 describe('Prime factors', () => {
-    it('finds the prime composition of the given number', () => {
+    it('knows what is a prime number', () => {
         expect(primeFactorsOf(2)).toEqual([2]);
-        expect(primeFactorsOf(4)).toEqual([2, 2]);
-        expect(primeFactorsOf(8)).toEqual([2, 2, 2]);
         expect(primeFactorsOf(3)).toEqual([3]);
-        expect(primeFactorsOf(9)).toEqual([3, 3]);
-        expect(primeFactorsOf(6)).toEqual([2, 3]);
-        expect(primeFactorsOf(25)).toEqual([5, 5]);
+    });
+
+    it('produces the same result to multiply the numbers in the output list', () => {
+        expect(primeFactorsOf(8)).toEqual([2, 2, 2]);
+    });
+
+    it('orders the prime factors from the smallest to the biggest', () => {
         expect(primeFactorsOf(1155)).toEqual([3, 5, 7, 11]);
     });
 });
