018ea64d4fed69545dc7e581cf25b10d70b0252e
Miguel Angel Perez Garcia <miguelangel.perez.dev@gmail.com>
2025-02-20 08:52:30 +0000

    ref: extract prime factors methods.

--- src/prime_factors/primeFactors.ts
+++ src/prime_factors/primeFactors.ts
@@ -1,3 +1,18 @@
+export function primeFactorsOf(number: number) {
+    checkForPositiveNumber(number);
+    return primeFactors(number);
+}
+
+function checkForPositiveNumber(number: number) {
+    if (number < 1) throw new Error('Only positive numbers are allowed');
+}
+
+function primeFactors(number: number) {
+    let prime = findSmallestPrime(number);
+    const remainder = number / prime;
+    return remainder <= 1 ? [prime] : [prime].concat(primeFactorsOf(remainder));
+}
+
 function findSmallestPrime(number: number) {
     if (number === 1) return 1;
     let factor = 2;
@@ -5,11 +20,4 @@ function findSmallestPrime(number: number) {
         ++factor;
     }
     return factor;
-}
-
-export function primeFactorsOf(number: number) {
-    if (number < 1) throw new Error('Only positive numbers are allowed');
-    let prime = findSmallestPrime(number);
-    const remainder = number / prime;
-    return remainder <= 1 ? [prime] : [prime].concat(primeFactorsOf(remainder));
 }
