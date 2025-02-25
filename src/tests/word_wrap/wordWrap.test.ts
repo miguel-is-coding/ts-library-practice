9045723ee7ace0067bd5d7d74d2990e3bd95f584
Miguel Angel Perez Garcia <miguelangel.perez.dev@gmail.com>
2025-02-25 07:38:49 +0000

    green: makes every single line of text fit the column width.

--- src/tests/word_wrap/wordWrap.test.ts
+++ src/tests/word_wrap/wordWrap.test.ts
@@ -18,19 +18,28 @@
 
 function wordWrap(text: string, columnWidth: number) {
     if (text.length <= columnWidth) {
-        return text
+        return text;
     }
-    const wrappedText = text.substring(0, columnWidth) + '\n';
-    const unwrappedText = text.substring(columnWidth);
-    return wrappedText.concat(wordWrap(unwrappedText, columnWidth))
+    let wrappedText: string;
+    let unwrappedText: string;
+    if (text.indexOf(' ') > -1 && text.indexOf(' ') < columnWidth) {
+        wrappedText = text.substring(0, text.indexOf(' ')).concat('\n');
+        unwrappedText = text.substring(text.indexOf(' ') + 1);
+    } else {
+        wrappedText = text.substring(0, columnWidth) + '\n';
+        unwrappedText = text.substring(columnWidth);
+    }
+    return wrappedText.concat(wordWrap(unwrappedText, columnWidth));
 }
 
 describe('Word wrap', () => {
     it('makes every single line of text fit the column width', () => {
-        expect(wordWrap('', 5)).toBe('')
-        expect(wordWrap('hello', 5)).toBe('hello')
-        expect(wordWrap('longword', 4)).toBe('long\nword')
-        expect(wordWrap('reallylongword', 4)).toBe('real\nlylo\nngwo\nrd')
-        expect(wordWrap('abc def', 4)).toBe('abc\ndef')
+        expect(wordWrap('', 5)).toBe('');
+        expect(wordWrap('hello', 5)).toBe('hello');
+        expect(wordWrap('longword', 4)).toBe('long\nword');
+        expect(wordWrap('reallylongword', 4)).toBe('real\nlylo\nngwo\nrd');
+        expect(wordWrap('abc def', 4)).toBe('abc\ndef');
+        expect(wordWrap('abc def ghi', 4)).toBe('abc\ndef\nghi');
+        expect(wordWrap(' abcd', 4)).toBe('\nabcd');
     });
 });
