e7abdc69c14c06c87cd2bbd0a99ec376a8f12ff6
Miguel Angel Perez Garcia <miguelangel.perez.dev@gmail.com>
2025-02-27 08:34:41 +0000

    ref: inline word wrap public method to wrappable text value object.

--- src/tests/word_wrap/wordWrap.test.ts
+++ src/tests/word_wrap/wordWrap.test.ts
@@ -1,27 +1,28 @@
-import {wordWrap} from "../../word_wrap/wordWrap";
+import {WrappableText} from "../../word_wrap/WrappableText";
+import {ColumnWidth} from "../../word_wrap/columnWidth";
 
 describe('Word wrap', () => {
     it('makes an empty text when there is no text', () => {
-        expect(wordWrap(null, 4)).toEqual({text: ''});
-        expect(wordWrap(undefined, 4)).toEqual({text: ''});
+        expect(WrappableText.create(null).wordWrap(ColumnWidth.create(4))).toEqual({text: ''});
+        expect(WrappableText.create(undefined).wordWrap(ColumnWidth.create(4))).toEqual({text: ''});
     });
 
     it('does not wrap text when the text fits the column width', () => {
-        expect(wordWrap('', 5)).toEqual({text: ''});
-        expect(wordWrap('hello', 5)).toEqual({text: 'hello'});
+        expect(WrappableText.create('').wordWrap(ColumnWidth.create(5))).toEqual({text: ''});
+        expect(WrappableText.create('hello').wordWrap(ColumnWidth.create(5))).toEqual({text: 'hello'});
     });
 
     it('add break lines when the text does not fit the column width', () => {
-        expect(wordWrap('reallylongword', 4)).toEqual({text: 'real\nlylo\nngwo\nrd'});
+        expect(WrappableText.create('reallylongword').wordWrap(ColumnWidth.create(4))).toEqual({text: 'real\nlylo\nngwo\nrd'});
     });
 
     it('break lines are preferred to be added in spaces', () => {
-        expect(wordWrap('abc def', 4)).toEqual({text: 'abc\ndef'});
-        expect(wordWrap('abc def ghi', 4)).toEqual({text: 'abc\ndef\nghi'});
-        expect(wordWrap(' abcd', 4)).toEqual({text: '\nabcd'});
+        expect(WrappableText.create('abc def').wordWrap(ColumnWidth.create(4))).toEqual({text: 'abc\ndef'});
+        expect(WrappableText.create('abc def ghi').wordWrap(ColumnWidth.create(4))).toEqual({text: 'abc\ndef\nghi'});
+        expect(WrappableText.create(' abcd').wordWrap(ColumnWidth.create(4))).toEqual({text: '\nabcd'});
     });
 
     it('cannot have a negative column width', () => {
-        expect(() => wordWrap('hello', -5).value()).toThrow('Column width cannot be negative');
+        expect(() => WrappableText.create('hello').wordWrap(ColumnWidth.create(-5)).value()).toThrow('Column width cannot be negative');
     });
 });
--- src/word_wrap/wordWrap.ts
+++
@@ -1,6 +0,0 @@
-import {ColumnWidth} from "./columnWidth";
-import {WrappableText} from "./WrappableText";
-
-export function wordWrap(text: string, columnWidth: number) {
-    return WrappableText.create(text).wordWrap(ColumnWidth.create(columnWidth))
-}
