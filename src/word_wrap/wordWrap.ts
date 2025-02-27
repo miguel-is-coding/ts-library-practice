6e2cb7583fb44a71d4a4cfda3618ba54fc2dd10c
Miguel Angel Perez Garcia <miguelangel.perez.dev@gmail.com>
2025-02-27 07:22:23 +0000

    ref: extract word wrap to separate file.

---
+++ src/word_wrap/wordWrap.ts
@@ -0,0 +1,49 @@
+function wordWrapOld(text: string, columnWidth: number) {
+    if (columnWidth < 0) {
+        throw new Error('Column width cannot be negative')
+    }
+    if (text == null) {
+        return '';
+    }
+    if (text.length <= columnWidth) {
+        return text;
+    }
+    const wrapIndex = getWrapIndex(text, columnWidth);
+    const unwrapIndex = getUnwrapIndex(text, columnWidth);
+    const wrappedText = text.substring(0, wrapIndex) + '\n';
+    const unwrappedText = text.substring(unwrapIndex);
+    return wrappedText.concat(wordWrapOld(unwrappedText, columnWidth));
+}
+
+function getWrapIndex(text: string, columnWidth: number) {
+    const spaceCharIndex = text.indexOf(' ');
+    const canWrapBySpace = spaceCharIndex > -1 && spaceCharIndex < columnWidth;
+    return canWrapBySpace ? spaceCharIndex : columnWidth;
+}
+
+function getUnwrapIndex(text: string, columnWidth: number) {
+    const spaceCharIndex = text.indexOf(' ');
+    const canWrapBySpace = spaceCharIndex > -1 && spaceCharIndex < columnWidth;
+    return canWrapBySpace ? spaceCharIndex + 1 : columnWidth;
+}
+
+export function wordWrap(text: string, columnWidth: number) {
+    return wordWrapNoPrimitives(text, columnWidth)
+}
+
+function wordWrapNoPrimitives(text: string, columnWidth: number) {
+    if (columnWidth < 0) {
+        throw new Error('Column width cannot be negative')
+    }
+    if (text == null) {
+        return '';
+    }
+    if (text.length <= columnWidth) {
+        return text;
+    }
+    const wrapIndex = getWrapIndex(text, columnWidth);
+    const unwrapIndex = getUnwrapIndex(text, columnWidth);
+    const wrappedText = text.substring(0, wrapIndex) + '\n';
+    const unwrappedText = text.substring(unwrapIndex);
+    return wrappedText.concat(wordWrapOld(unwrappedText, columnWidth));
+}
