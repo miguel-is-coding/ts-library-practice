6e18ed65f865888c680ebb136adf54a3e50b438f
Miguel Angel Perez Garcia <miguelangel.perez.dev@gmail.com>
2025-03-06 07:03:16 +0000

    red(acceptance): prints an account statement with its transactions.

---
+++ src/tests/banking_kata/printStatement.test.ts
@@ -0,0 +1,19 @@
+import {Console} from "../../banking_kata/console";
+import {Account} from "../../banking_kata/account";
+
+describe('Print statement', () => {
+    const console = new Console();
+    const consoleSpy = jest.spyOn(console, 'log');
+    const account = new Account();
+
+    it('prints an account statement with its transactions', () => {
+        account.deposit(1000);
+        account.withdraw(500);
+        account.deposit(2000);
+
+        expect(consoleSpy).toHaveBeenCalledWith('Date | Amount | Balance');
+        expect(consoleSpy).toHaveBeenCalledWith('05/03/2025 | 2000 | 2500');
+        expect(consoleSpy).toHaveBeenCalledWith('10/02/2025 | -500 | 500');
+        expect(consoleSpy).toHaveBeenCalledWith('06/02/2025 | 1000 | 1000');
+    });
+});
