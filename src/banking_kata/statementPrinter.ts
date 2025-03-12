a5c6fc288a1727951aade314029186517f773352
Miguel Angel Perez Garcia <miguelangel.perez.dev@gmail.com>
2025-03-12 08:10:09 +0000

    green: prints a statement of account including a given transaction.

--- src/banking_kata/statementPrinter.ts
+++ src/banking_kata/statementPrinter.ts
@@ -9,6 +9,14 @@ export class StatementPrinter {
 
     print(transactions: Transaction[]) {
         this.console.log(this.header);
+        if (transactions.length > 0) {
+            const transaction = transactions[0];
+            let currentBalance = 0;
+            currentBalance += transaction.amount;
+            const formattedAmount = transaction.amount.toFixed(2);
+            const formattedBalance = currentBalance.toFixed(2);
+            this.console.log(`${transaction.date} | ${formattedAmount} | ${formattedBalance}`)
+        }
     }
 }
 
