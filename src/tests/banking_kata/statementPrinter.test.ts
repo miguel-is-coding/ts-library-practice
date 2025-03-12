d6c33de405c963aa47779457e6e5959f7fd92a6f
Miguel Angel Perez Garcia <miguelangel.perez.dev@gmail.com>
2025-03-12 08:14:05 +0000

    red: prints a statement of account including multiple transactions.

--- src/tests/banking_kata/statementPrinter.test.ts
+++ src/tests/banking_kata/statementPrinter.test.ts
@@ -17,8 +17,24 @@ describe('The Statement Printer', () => {
         const transactions = [new Transaction('12/03/2025', 100)]
 
         statementPrinter.print(transactions);
+
+        expect(consoleSpy).toHaveBeenCalledWith('Date | Amount | Balance');
+        expect(consoleSpy).toHaveBeenCalledWith('12/03/2025 | 100.00 | 100.00');
+    });
+
+    it('prints a statement of account including multiple transactions', () => {
+        const statementPrinter = new StatementPrinter(console);
+        const transactions = [
+            new Transaction('12/03/2025', 100),
+            new Transaction('13/03/2025', 500),
+            new Transaction('14/03/2025', -200),
+        ]
+
+        statementPrinter.print(transactions);
         
         expect(consoleSpy).toHaveBeenCalledWith('Date | Amount | Balance');
+        expect(consoleSpy).toHaveBeenCalledWith('14/03/2025 | -200.00 | 400.00');
+        expect(consoleSpy).toHaveBeenCalledWith('13/03/2025 | 500.00 | 600.00');
         expect(consoleSpy).toHaveBeenCalledWith('12/03/2025 | 100.00 | 100.00');
     });
 });
