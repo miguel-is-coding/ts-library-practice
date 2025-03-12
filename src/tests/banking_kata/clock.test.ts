fb904da3312c669cb27fc8a619c65231b42c8e18
Miguel Angel Perez Garcia <miguelangel.perez.dev@gmail.com>
2025-03-12 07:54:58 +0000

    green: the clock gets the current date in dd/mm/yyyy format.

--- src/tests/banking_kata/clock.test.ts
+++ src/tests/banking_kata/clock.test.ts
@@ -2,8 +2,14 @@ import {Clock} from "../../banking_kata/clock";
 
 describe('The clock', () => {
     it('gets the current date in dd/mm/yyyy format', () => {
-        const clock = new Clock();
+        const clock = new FakeClock();
 
         expect(clock.todayFormatted()).toEqual('12/03/2025')
     });
 });
+
+class FakeClock extends Clock {
+    protected today(): Date {
+        return new Date('2025-03-12');
+    }
+}
